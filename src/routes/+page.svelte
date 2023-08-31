fd149e548b5e4b5f2275ae4ab1f12850db4cccbc
Nathan Griffiths <ngriffiths@postmedia.com>
2023-08-30 21:26:52 -0700

    added dot files & shift-book font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     @import './css/page-styles.css';
 </style>
 
-<h1>SvelteKit Template</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>VS SvelteKit Template</h1>
+<p class="subhead">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
