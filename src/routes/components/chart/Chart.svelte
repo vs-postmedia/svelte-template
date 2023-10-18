cf9b73ad135255f057ab159003f5b18c393bf655
Nathan Griffiths <ngriffiths@postmedia.com>
2023-10-17 21:17:07 -0700

    hmmm... whole lotta changes

--- src/routes/components/chart/Chart.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    export let data = [];
-    export let value = '';
-</script>
-
-<pre>{`CHART: Resolved data = ${JSON.stringify(data)}`}</pre>
-<pre>CHART: Combobox value = {value || 'No selection'}</pre>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
+  base: '/svelte-template/',
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      "$components": path.resolve('./src/components'),
+      "$data": path.resolve("./src/data"),
+	    "$css": path.resolve("./src/css"),
+	    "$fonts": path.resolve("./static/fonts")
+    }
+  }
 });
